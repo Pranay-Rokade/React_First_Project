0df45e451f87bc5b2b2a0ef766e39539f710e022
Pranay-Rokade <pranayrokade86@gmail.com>
2024-12-22 16:25:02 +0530

    Adding More Logic to TextUtils

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,18 +6,33 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText);
     }
+    const handleLoClick = () => {
+        // console.log("Lowercase was clicked: "+ text);
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
     const handleOnChange = (event) => {
         // console.log("On Change");
         setText(event.target.value);
     }
-    const [text, setText] = useState('Enter your text here');
+    const [text, setText] = useState('');
   return (
-    <div>
+    <>
+    <div className="container">
         <h1>{props.heading}</h1>
         <div className="mb-3">
             <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-success" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-success mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
     </div>
+    <div className="container my-3">
+        <h2>Your text summary</h2>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h3>Preview</h3>
+        <p>{text}</p>   
+    </div>
+    </>
   )
 }
