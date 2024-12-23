73eae32454a29dad4b5a536e7192131dc5f9c778
Pranay-Rokade <pranayrokade86@gmail.com>
2024-12-23 19:11:22 +0530

    Created Dark Mode option using useState hook, Added auto dismissed Alerts and enhanced TextUtils

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import './App.css';
+import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, {useState} from 'react';
+
 
 function App() {
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#010d00';
+      showAlert("Dark mode has been enabled", "success");
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+    }
+  }
   return (
   <>
-  <Navbar title="TextUtils" aboutText="About"/>
+  <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+  <Alert alert={alert}/>
   <div className="container my-3">
-  <TextForm heading="Enter your Text to analyse"/>
+  <TextForm showAlert={showAlert} heading="Enter your Text to analyse" mode={mode} />
   </div>
   </>  
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,36 +2,61 @@ import React, {useState} from 'react'
 
 export default function TextForm(props) {
     const handleUpClick = () => {
-        // console.log("Uppercase was clicked: "+ text);
         let newText = text.toUpperCase();
         setText(newText);
+        props.showAlert("Converted to Uppercase", "success");
     }
+
     const handleLoClick = () => {
-        // console.log("Lowercase was clicked: "+ text);
         let newText = text.toLowerCase();
         setText(newText);
+        props.showAlert("Converted to Lowercase", "success");
     }
+
+    const handleClearClick = () => {
+        let newText = '';
+        setText(newText);
+        props.showAlert("Text Cleared", "success");
+    }
+
+    const handleCopy = () => {
+        var text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+        props.showAlert("Text Copied", "success");
+    }
+
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+        props.showAlert("Extra Spaces Removed", "success");
+    }
+
     const handleOnChange = (event) => {
-        // console.log("On Change");
         setText(event.target.value);
     }
+
     const [text, setText] = useState('');
+    // text = "new text"; // Wrong way to change the state
   return (
     <>
-    <div className="container">
+    <div className="container" style={{color: props.mode === 'dark'?'white':'#010d00'}}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark'?'#010d00':'white', color: props.mode === 'dark'?'white':'#010d00'}} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-success" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-success mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
         <button className="btn btn-success mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
+        <button className="btn btn-success mx-2" onClick={handleClearClick}>Clear Text</button>
+        <button className="btn btn-success mx-2" onClick={handleCopy}>Copy Text</button>
+        <button className="btn btn-success mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
     </div>
-    <div className="container my-3">
+    <div className="container my-3" style={{color: props.mode === 'dark'?'white':'#010d00'}}>
         <h2>Your text summary</h2>
         <p>{text.split(" ").length} words and {text.length} characters</p>
         <p>{0.008 * text.split(" ").length} Minutes read</p>
         <h3>Preview</h3>
-        <p>{text}</p>   
+        <p>{text.length>0?text:"Nothing to Preview here."}</p>   
     </div>
     </>
   )
