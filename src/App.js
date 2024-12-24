75fa0dde16c5a1eb8ca56562a46d1dc0d673ce98
Pranay-Rokade <pranayrokade86@gmail.com>
2024-12-24 21:03:01 +0530

    Added React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import About from './components/About';
 import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, {useState} from 'react';
+import{BrowserRouter as Router,Routes,Route} from "react-router-dom";
 
 
 function App() {
@@ -33,11 +35,16 @@ function App() {
   }
   return (
   <>
+  <Router>
   <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
   <Alert alert={alert}/>
   <div className="container my-3">
-  <TextForm showAlert={showAlert} heading="Enter your Text to analyse" mode={mode} />
+  <Routes>
+      <Route exact path="/about" element={<About/>}/>
+      <Route exact path="/" element={<TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert} />} />
+  </Routes>
   </div>
+  </Router>
   </>  
   );
 }
