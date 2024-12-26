058b55b31bd39322eed5dce6c445253a7d850a9f
Pranay-Rokade <pranayrokade86@gmail.com>
2024-12-26 20:05:38 +0530

    Solved Minor Errors

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,6 @@
 import React from 'react'
 
 export default function About(props) {
-    // const [myStyle, setMyStyle] = useState({
-    //     color: 'black',
-    //     backgroundColor: 'white'
-    // })
     let myStyle = {
         color: props.mode === 'dark'?'white':'#010d00',
         backgroundColor: props.mode === 'dark'?'#010d00':'white',
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -20,10 +20,7 @@ export default function TextForm(props) {
     }
 
     const handleCopy = () => {
-        var text = document.getElementById("myBox");
-        text.select();
-        navigator.clipboard.writeText(text.value);
-        document.getSelection().removeAllRanges();
+        navigator.clipboard.writeText(text);
         props.showAlert("Text Copied", "success");
     }
 
@@ -54,7 +51,7 @@ export default function TextForm(props) {
     </div>
     <div className="container my-3" style={{color: props.mode === 'dark'?'white':'#010d00'}}>
         <h2>Your text summary</h2>
-        <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+        <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
         <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
         <h3>Preview</h3>
         <p>{text.length>0?text:"Nothing to Preview here!!!"}</p>   
